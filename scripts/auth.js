a9dd01c301bede15f7d433912be6a992e02a322e
Muhammad Buhari <gmbdairy@gmail.com>
2020-12-24 12:48:03 +0100

    admin function

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,15 @@
-// get data
+// const { functions } = require("lodash");
+
+// add admin cloud function
+const adminForm = document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const adminEmail = document.querySelector('#admin-email').value;
+    const addAdminRole = functions.httpsCallable('addAdminRole');
+    addAdminRole({email: adminEmail}).then(result => {
+        console.log(result);
+    });
+});
 
 
 // listen for auth status changes
