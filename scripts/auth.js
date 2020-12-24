290cace13d477de6b3432d874ce43c34cb7da8ee
Muhammad Buhari <gmbdairy@gmail.com>
2020-12-24 12:04:14 +0100

    first commit

---
+++ scripts/auth.js
@@ -0,0 +1,83 @@
+// get data
+
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+   if(user){
+    db.collection('guides').onSnapshot(snapshot => {
+        setupGuides(snapshot.docs);
+        setupUI(user);
+    }, err => {
+        console.log(err.message)
+    });
+    }else{
+        setupUI(null);
+        setupGuides([]);
+    }
+});
+
+// Create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        const modal =document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err.messag)
+    })
+});
+
+// Signup
+const signUpForm = document.querySelector('#signup-form');
+signUpForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    // get user info
+    const email = signUpForm['signup-email'].value;
+    const password = signUpForm['signup-password'].value;
+    const displayName = signUpForm['displayName'].value;
+    
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signUpForm['signup-bio'].value,
+            displayName: displayName
+        }); 
+    }).then(() => {
+        const modal =document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signUpForm.reset();  
+    });
+}) 
+
+// logout
+const logout=document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        // console.log('User signed out');
+    });
+});
+
+// login form
+loginForm=document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        // console.log(cred.user);
+    // close login and reset form
+    const modal =document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
